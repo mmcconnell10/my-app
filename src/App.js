52b97d817081fb7944a7bf9cdfab255397c21c6f
Molly McConnell <molly.m@wutzgoodapp.com>
2020-02-12 17:59:38 -0600

    chore(styling): making code readable

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
    
       return (
     <Router>
-      <div>
+      
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -30,16 +30,8 @@ class App extends React.Component {
             <Link to="/exercise">Exercise</Link>
           </li>
         </ul>
-
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
+        
         <Switch>
           <Route exact path="/">
             <Home />
@@ -51,7 +43,7 @@ class App extends React.Component {
             <Exercise />
           </Route>
         </Switch>
-      </div>
+      
     </Router>
   );
 }

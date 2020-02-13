27dc980d4c9179542abb57fe8b6ab9a76c9be158
Molly McConnell <molly.m@wutzgoodapp.com>
2020-02-13 10:54:33 -0600

    feat(header): header with navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 import Home from './home';
 import Form from './form';
 import Exercise from './exercise';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 
@@ -17,29 +20,36 @@ class App extends React.Component {
   render(){
    
       return (
-    <Router>
-      
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/form">Form</Link>
-          </li>
-          <li>
-            <Link to="/exercise">Exercise</Link>
-          </li>
-        </ul>
-        <hr />
-
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/form" component={Form}/>
-          <Route path="/exercise" component={Exercise} exact />
-          <Route component={Error} />
-        </Switch>
-      
-    </Router>
+        <div>
+        <Navbar bg="dark" expand="lg">
+          <Navbar.Brand href="#home" style={{color:'white'}}>
+            <img
+              alt="logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+           Molly's App
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" >
+            <Nav className="mr-auto">
+              <Nav.Link href="/" style={{color:'white'}}>Home</Nav.Link>
+              <Nav.Link href="/form" style={{color:'white'}}>Form</Nav.Link>
+              <Nav.Link href="/exercise" style={{color:'white'}}>Exercise</Nav.Link>
+            </Nav>       
+          </Navbar.Collapse>
+        </Navbar>
+        <Router>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/form" component={Form}/>
+              <Route path="/exercise" component={Exercise} exact />
+              <Route component={Error} />
+            </Switch>
+        </Router>
+    </div>
   );
 }
   }
